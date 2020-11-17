5f53d002c88eb588d0967e01da7310344f905502
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2020-11-17 17:58:22 +0900

    to do: click, drag event

--- index.js
+++ index.js
@@ -4,26 +4,18 @@ const degree2Radian = (degrees) => degrees * (Math.PI / 180);
 
 let width, height, mouseX, mouseY, centerX, centerY;
 
-// NOTE: Set gradient color for stroke
-let gradient = ctx.createLinearGradient(250, 0, 0, 500);
-gradient.addColorStop(1, "#DCF3BB");
-gradient.addColorStop(0, "#758E50");
-
-// NOTE: Size for circle slider
 const circleSize = 300;
-// NOTE: slider width
 const thickness = 5;
-// NOTE: Circle size on Slider
 const draggingCircleSize = 50;
 
 window.addEventListener("load", (_) => {
-  canvas.width = 1000;
-  canvas.height = 1000;
+  canvas.width = 900;
+  canvas.height = 900;
 
   width = canvas.width;
   height = canvas.height;
-  centerX = width / 2 / 2;
-  centerY = height / 2 / 2;
+  centerX = (width - circleSize) / 2;
+  centerY = (height - circleSize) / 2;
 
   window.requestAnimationFrame(render);
 });
@@ -39,43 +31,58 @@ function render() {
 
   ctx.fillStyle = "black";
 
-  // NOTE: Calculate the angle made by mouse position and center position.
+  // 마우스 위치-센터 (원 기준으로 마우스가 있는 곳) 각도를 계산
+  // 썸의 위치 계산
   const theta = Math.atan2(mouseY - centerY, mouseX - centerX);
 
   for (let i = 0; i < 360; i += step) {
     const x = Math.cos(degree2Radian(i)) * circleSize;
     const y = Math.sin(degree2Radian(i)) * circleSize;
 
-    // NOTE: Draw debug circles
+    // 점선
     ctx.beginPath();
     ctx.strokeStyle = "black";
-    ctx.setLineDash([5, 15]);
+    ctx.setLineDash([7, 10]);
+    ctx.lineWidth = 1;
     ctx.arc(width / 2, height / 2, circleSize, 0, 360);
-    //    ctx.arc(width / 2 + x, height / 2 + y, 10, 0, 2 * Math.PI);
     ctx.stroke();
 
-    // NOTE: Draw a circle slider
+    //원 시작점에 애기 원 그리기
     ctx.beginPath();
+    ctx.arc(width / 2, (width - circleSize) / 4, 10, 0, 360);
+    ctx.lineWidth = 5;
     ctx.setLineDash([]);
-    ctx.arc(width / 2, height / 2, circleSize, degree2Radian(-90), theta);
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = thickness;
-    ctx.lineCap = "round";
     ctx.stroke();
-
-    // NOTE: Draw a circle on the slider
-    const cx = Math.cos(theta) * circleSize;
-    const cy = Math.sin(theta) * circleSize;
-    ctx.beginPath();
-    ctx.arc(
-      width / 2 + cx,
-      height / 2 + cy,
-      draggingCircleSize,
-      0,
-      2 * Math.PI
-    );
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "white";
     ctx.fill();
+
+    // 마우스 따라서 그려지는 선
+    function drawLine() {
+      ctx.beginPath();
+
+      ctx.arc(width / 2, height / 2, circleSize, degree2Radian(-90), theta);
+      ctx.strokeStyle = "black";
+      ctx.lineCap = "round";
+      ctx.stroke();
+    }
+
+    // thumb 손잡이
+    function drawThumb() {
+      const cx = Math.cos(theta) * circleSize;
+      const cy = Math.sin(theta) * circleSize;
+      ctx.beginPath();
+      ctx.arc(
+        width / 2 + cx,
+        height / 2 + cy,
+        draggingCircleSize,
+        0,
+        2 * Math.PI
+      );
+      ctx.fillStyle = "black";
+      ctx.fill();
+    }
+    drawLine();
+    drawThumb();
   }
 
   window.requestAnimationFrame(render);
